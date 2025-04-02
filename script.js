d4b7fc0d08b93cdccce706bac7e2571f0df7a1d9
UceanEnt <uceanentertainment@gmail.com>
2025-04-02 08:03:18 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesBtn').addEventListener('click', function() {
+  window.location.href = 'https://wa.me/8765716535?text=Hi%20Gavin,%20I%20would%20love%20to%20be%20your%20friend!';
+});
+
+document.getElementById('noBtn').addEventListener('click', function() {
+  alert('No worries! Hope we can still be friends.');
+});
